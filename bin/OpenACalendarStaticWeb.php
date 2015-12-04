91eec8ada15bd674711958b7d01f2baa8cc93a06
James Baster <james@jarofgreen.co.uk>
2015-12-04 20:52:12 +0000

    Travis can execute demo data and make sure it passes to! Thanks exit codes!

--- bin/OpenACalendarStaticWeb.php
+++ bin/OpenACalendarStaticWeb.php
@@ -45,37 +45,41 @@ function setConfig(\openacalendar\staticweb\Site $site, $opts) {
 }
 
 if (isset($opts['build'])) {
-	if (!isset($opts['out']) || !$opts['out']) {
-		print "Build but no out set?\n";
-		die();
-	}
-	if (!isset($opts['site']) || !$opts['site']) {
-		print "Build but no site set?\n";
-		die();
-	}
+    if (!isset($opts['out']) || !$opts['out']) {
+        print "Build but no out set?\n";
+        die();
+    }
+    if (!isset($opts['site']) || !$opts['site']) {
+        print "Build but no site set?\n";
+        die();
+    }
 
-	$site = new \openacalendar\staticweb\Site($app, $opts['site']);
-	setConfig($site, $opts);
+    $site = new \openacalendar\staticweb\Site($app, $opts['site']);
+    setConfig($site, $opts);
 
-	if ($site->getWarnings()) {
-		print "Warnings:\n\n";
-		foreach($site->getWarnings() as $warning) {
-			print get_class($warning)."\n\n";
-		}
-	}
+    if ($site->getWarnings()) {
+        print "Warnings:\n\n";
+        foreach($site->getWarnings() as $warning) {
+            print get_class($warning)."\n\n";
+        }
+    }
 
-	if ($site->getErrors()) {
-		print "ERRORS:\n\n";
-		foreach($site->getErrors() as $error) {
-			print get_class($error)."\n\n";
-		}
-	} else {
+    if ($site->getErrors()) {
+        print "ERRORS:\n\n";
+        foreach($site->getErrors() as $error) {
+            print get_class($error)."\n\n";
+        }
 
-		print "\n";
+        // Exit failure
+        exit(1);
+    } else {
 
-		$site->write($opts['out']);
+        $site->write($opts['out']);
 
-	}
+        // Exit Success
+        exit(0);
+
+    }
 
 
 }
